cf0b39f6e5b576f3e51090306f3008a525768354
Riyaa Thejaswi <riyaa.thejaswi@education.nsw.gov.au>
2026-09-21 09:14:07 +1000

    first commit

---
+++ rocket.py
@@ -0,0 +1,6 @@
+#This script runs a short countdown from 10 down to 0 (not including) before printing the message "Blast Off"
+import time
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+print("Blast Off 💥🚀")
