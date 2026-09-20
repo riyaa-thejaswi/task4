a96eee852233654920a18fc2d6157ba7d2d5fdb6
Riyaa Thejaswi <riyaa.thejaswi@education.nsw.gov.au>
2026-09-21 09:27:18 +1000

    Added ownership line

--- rocket.py
+++ rocket.py
@@ -1,5 +1,6 @@
 #This script runs a short countdown from 10 down to 0 (not including) before printing the message "Blast Off"
 import time
+print("Written by: Riyaa")
 for i in range(10, 0, -1):
     print(i)
     time.sleep(1)
